5c273ee3088ae0900449f3e4df2094193c152d77
Robert Dahlström <robert@diabol.se>
2018-11-29 09:52:51 +0100

    New DockerHubSpider and tweaks to existing spiders

--- versions/spiders/__init__.py
+++ versions/spiders/__init__.py
@@ -13,34 +13,31 @@ Various 'spiders' that know how to retrieve version information from different s
 
 
 class BitbucketReleaseSpider(object):
-    def __init__(self, **kwargs):
+    """
+    Retrieve version from a Bitbucket tags (only looking at top two ones, discarding latest)
+    Assumes tag list is sorted in a meaningful manner when it relates to versions
+    """
+    def __init__(self, owner, repository):
         api = "https://api.bitbucket.org/2.0/repositories/{owner}/{repository}/refs/tags?sort=-name"
-        self.url = api.format(owner=kwargs['owner'], repository=kwargs['repository'])
+        self.url = api.format(owner=owner, repository=repository)
 
     def get_version(self):
-        # https://api.bitbucket.org/2.0/repositories/atlassian/docker-atlassian-bitbucket-server/refs/tags?sort=-name
-        # The first item should be latest
-        # after that should be the last version, Phew
-
-        # https://api.bitbucket.org/2.0/repositories/atlassian/docker-atlassian-bitbucket-server/refs/tags/latest
         response = requests.get(self.url)
         response.raise_for_status()
         data = response.json()
-        latest = data['values'][0]
-        latest_version = data['values'][1]
+        candidate = data['values'][0]
+        if candidate['name'] == 'latest':
+            candidate = data['values'][1]
 
-        if latest['target']['hash'] != latest_version['target']['hash']:
-            raise ValueError('Expected latest tag followed by last release tag but found different commits')
-
-        return latest_version['name']
+        return candidate['name']
 
 
 class DockerfileSpider(object):
     """
     Retrieve version from a local Dockerfile (parse the Dockerfile FROM entry)
     """
-    def __init__(self, **kwargs):
-        self.path = kwargs['path']
+    def __init__(self, path):
+        self.path = path
         self.re = re.compile('^FROM .*:v?(\d+.*)$')
  
     def get_version(self):
@@ -57,13 +54,35 @@ class DockerfileSpider(object):
         raise ValueError("No version found in {path}".format(path=self.path))
 
 
+class DockerHubSpider(object):
+    """
+    Retrieve version from Docker hub tags list (only look at the top two ones, discarding latest)
+    """
+    def __init__(self, owner, name):
+        self.owner = owner
+        self.name = name
+        api = "https://registry.hub.docker.com/v2/repositories/{owner}/{name}/tags/"
+        self.url = api.format(owner=self.owner, name=self.name)
+
+    def get_version(self):
+        response = requests.get(self.url)
+        response.raise_for_status()
+
+        data = response.json()
+        candidate = data['results'][0]
+        if candidate['name'] == 'latest':
+            candidate = data['results'][1]
+
+        return candidate['name']
+
+
 class GithubReleaseSpider(object):
     """
     Retrieve version for the latest release of a Github project using the Github API
     """
-    def __init__(self, **kwargs):
+    def __init__(self, owner, repository):
         api = 'https://api.github.com/repos/{owner}/{repository}/releases/latest'
-        self.url = api.format(owner=kwargs['owner'], repository=kwargs['repository'])
+        self.url = api.format(owner=owner, repository=repository)
 
     def get_version(self):
         """Response contains a Github release API response and since we request latest the tag_name will correspond
@@ -96,13 +115,14 @@ class KubernetesVersionLabelSpider(object):
     """
     Retrieve version from a running k8s deployment assuming it's been labeled with app.kubernetes.io/version
     """
-    def __init__(self, name, namespace):
+    def __init__(self, item, name, namespace):
+        self.item = item
         self.name = name
         self.namespace = namespace
 
     def get_version(self):
-        kubectl_command = "kubectl get deployment {name} -n {namespace} -o yaml".format(
-            name=self.name, namespace=self.namespace
+        kubectl_command = "kubectl get {item} {name} -n {namespace} -o yaml".format(
+            item=self.item, name=self.name, namespace=self.namespace
         )
         result = subprocess.run(kubectl_command, shell=True, check=True, stdout=subprocess.PIPE, encoding='utf-8')
         data = yaml.load(result.stdout)
@@ -134,8 +154,6 @@ class KubernetesImageVersionSpider(object):
 
             section = section[p]
 
-        # Here we expect section to contain the image only
-        # e.g. quay.io/prometheus/prometheus:v2.2.1
-        # Next: parse version from that string
+        # Here we expect section to contain the image only, e.g. "quay.io/prometheus/prometheus:v2.2.1"
         (_, version) = section.split(':')
         return version.lstrip('v')
