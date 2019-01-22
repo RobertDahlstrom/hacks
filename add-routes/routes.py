7d7a8364a20ad4f5b1b5be2e36071895389efdc9
Robert Dahlström <robert@diabol.se>
2019-01-22 14:34:57 +0100

    Improved config to allow 'automated' gateway detection

--- add-routes/routes.py
+++ add-routes/routes.py
@@ -2,26 +2,30 @@
 
 # Given list of addresses, grab their ip's and add them to the routing table
 import argparse
+import re
 import socket
 import subprocess
+import yaml
 
 
-def get_ips_to_route(file_with_host_names):
+def get_ips_to_route(hosts):
     ips = []
-    with open(file_with_host_names, "rb") as f:
-        for dirty_host in f:
-            host = dirty_host.strip().decode("utf-8")
-            if host.startswith('#'):
-                continue
-            ip = socket.gethostbyname(host)
-            ips.append(ip)
+    for host in hosts:
+        ip = socket.gethostbyname(host)
+        ips.append(ip)
     return ips
+
+
+def get_gateway_from_host_check(host):
+    result = subprocess.run(['route', '-n', 'get', host],
+                            check=True, capture_output=True, encoding='UTF-8')
+    match = re.search(r'^\s+gateway: (.*?)$', result.stdout, re.MULTILINE)
+    return match.group(1)  # Capture group 1 is gateway ip
 
 
 def add_route_for_ips(ips, gateway):
     for ip in ips:
-        result = subprocess.run(["sudo", "route", "-n", "add", "-net", "{ip}/32".format(ip=ip), gateway])
-        result.check_returncode()
+        subprocess.run(["sudo", "route", "-n", "add", "-net", "{ip}/32".format(ip=ip), gateway], check=True)
 
 
 if __name__ == '__main__':
@@ -29,12 +33,15 @@ if __name__ == '__main__':
         description="Simple wrapper around route that does dns names to routes mappings. "
                     "Useful over VPN tunnels when routes are missing."
     )
-    parser.add_argument('--gateway', required=True, help="The gateway to route requests through")
-    parser.add_argument('--hosts', default="hosts", help="One DNS host entry per line")
+    parser.add_argument('--config', default="routes.config.yaml", help="The config file to read")
     args = parser.parse_args()
 
-    ips = get_ips_to_route(args.hosts)
+    with open(args.config) as stream:
+        config = yaml.safe_load(stream)['routes']
+
+    ips = get_ips_to_route(config['hosts_to_route'])
 
     print("About to invoke sudo, please provide your password if prompted")
+    gateway = get_gateway_from_host_check(config['host_check_gateway'])
 
-    add_route_for_ips(ips, args.gateway)
+    add_route_for_ips(ips, gateway)
