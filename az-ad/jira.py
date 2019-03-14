efbe637b7a66e272d703439ffeea60fca4bc16f8
Robert Dahlström <robert@diabol.se>
2019-03-14 10:10:45 +0100

    Pretty print json output

--- az-ad/jira.py
+++ az-ad/jira.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import json
 import requests
 
 
@@ -8,7 +9,7 @@ def main(args):
     url = 'https://{args.host}/rest/api/2/user/search?username={args.search}&maxResults=1000'.format(args=args)
     request = requests.get(url, auth=(args.user, args.password))
     request.raise_for_status()
-    print(request.json())
+    print(json.dumps(request.json(), indent=4, separators=(',', ': ')))
 
 
 if __name__ == '__main__':
