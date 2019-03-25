b970bf3567569118d11f5f680e890183c9bf884b
Robert Dahlström <robert@diabol.se>
2019-03-25 13:41:23 +0100

    Support action 'in'

--- az-ad/adgroups.py
+++ az-ad/adgroups.py
@@ -5,28 +5,38 @@ import azure_wrapper
 
 
 def main(action, left, right):
-    # Grab members from left group
+    if action not in ['add', 'remove', 'in']:
+        raise ValueError("{action} is not a supported action".format(action=action))
+
+    count = 0
     left_members = azure_wrapper.get_users_in_group(left)
 
-    # For each left group member, apply action on right group
-    count = 0
-    for member in left_members:
-        print("Will {action} {mail} in {right}...".format(action=action, mail=member['mail'], right=right), end='')
-        if action == 'add':
-            azure_wrapper.add_group_member(right, member['objectId'])
-        elif action == 'remove':
-            azure_wrapper.remove_member_from_group(right, member['objectId'])
-        else:
-            raise ValueError("{action} is not a supported action".format(action=action))
-        count += 1
-        print('Done')
+    if action == 'in':
+        count = len(left_members)
+        right_members = azure_wrapper.get_users_in_group(right)
+
+        in_both = [x['objectId'] for x in left_members if x in right_members]
+        print("Members in both groups:")
+        for user_id in in_both:
+            print(user_id)
+    else:
+        for member in left_members:
+            print("Will {action} {mail} in {right}...".format(action=action, mail=member['mail'], right=right), end='')
+            if action == 'add':
+                azure_wrapper.add_group_member(right, member['objectId'])
+            elif action == 'remove':
+                azure_wrapper.remove_member_from_group(right, member['objectId'])
+            else:
+                raise ValueError("Internal error: {action} is supposed to be valid here".format(action=action))
+            count += 1
+            print('Done')
 
     print("{count} users processed".format(count=count))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--action", help="Export of active users in JIRA", required=True, choices=['add', 'remove'])
+    parser.add_argument("--action", help="Export of active users in JIRA", required=True, choices=['add', 'remove', 'in'])
     parser.add_argument("left", help="The left group to apply action with")
     parser.add_argument("right", help="The right group to apply action with")
     args = parser.parse_args()
